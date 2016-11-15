5fcf3c5427f556a8a3032149123e84dea233b99e
zys <zys@zysdeiMac.local>
2016-11-15 18:32:46 +0800

    Initial Commit

---
+++ YSGaussianViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YSGaussianViewDemo
+//
+//  Created by zys on 2016/11/15.
+//  Copyright © 2016年 张永帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YSGaussianViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YSGaussianViewDemo
+//
+//  Created by zys on 2016/11/15.
+//  Copyright © 2016年 张永帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
